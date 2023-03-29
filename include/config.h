8b79355ee687b6ef78d52a92e231935d1fc42c83
Chiro Liang <Chiro2001@163.com>
2023-03-29 17:45:56 +0800

    info

--- include/config.h
+++ include/config.h
@@ -11,8 +11,8 @@
 
 
 #ifdef TEST
-#define NET_IF_IP    \
-    {                   \
+#define NET_IF_IP          \
+    {                      \
         192, 168, 163, 103 \
     } //测试用网卡ip地址
 #define NET_IF_MAC                      \
@@ -20,16 +20,16 @@
         0x11, 0x22, 0x33, 0x44, 0x55, 0x66 \
     } //测试用网卡mac地址
 #else
-#define NET_IF_IP    \
-    {                   \
-        XX, XX, XX, XX \
+// -> pc.chiro.work
+#define NET_IF_IP        \
+    {                    \
+        10, 250, 122, 93 \
     } //自定义网卡ip地址
-#define NET_IF_MAC                      \
+#define NET_IF_MAC                         \
     {                                      \
-        0x00, 0x11, 0x22, 0x33, 0x44, 0x55 \
+        0x44, 0xe5, 0x17, 0xf9, 0xf1, 0xe6 \
     } //自定义网卡mac地址
-#endif 
-
+#endif
 
 
 #define ETHERNET_MAX_TRANSPORT_UNIT 1500 //以太网最大传输单元
